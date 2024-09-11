996db72896c221c64f3496fb9706b4a88dfb0439
Slava <visla.vvi@gmail.com>
2024-09-11 19:06:04 +0500

    cantor: a pair of combos to get into boot loader.

--- cantor/keymap.c
+++ cantor/keymap.c
@@ -48,6 +48,12 @@
 #define XX KC_NO
 #define OSM_SFT OSM(MOD_LSFT)
 
+/* */
+enum my_keycodes {
+  SWITCH_LANG = SAFE_RANGE,
+  XX_FAKE,
+};
+
 /* Layer names */
 enum my_layer_names {
   L_BOO,
@@ -71,17 +77,17 @@ enum my_layer_names {
 
 const uint16_t PROGMEM esc_combo[]   = {OSM_SFT, KC_SPACE, COMBO_END};
 const uint16_t PROGMEM mouse_combo[] = {OSL_SYM, KC_SPACE, COMBO_END};
+const uint16_t PROGMEM reset_combo_left[] = {XX_FAKE, OSL_SYM, COMBO_END};
+const uint16_t PROGMEM reset_combo_right[] = {KC_ENTER, XX_FAKE, COMBO_END};
 
 combo_t key_combos[] = {
   COMBO(esc_combo, KC_ESC),
   COMBO(mouse_combo, OSL_MOUSE),
+  COMBO(reset_combo_left, QK_BOOT),
+  COMBO(reset_combo_right, QK_BOOT),
 };
 
 /* */
-
-enum my_keycodes {
-  SWITCH_LANG = SAFE_RANGE,
-};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
@@ -105,12 +111,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_BOO] = LAYOUT_split_3x6_3(/* BOO LAYOUT
        QWE '   ,   u   c   v                        q   f   d   l   y   /
        --- a   o   e   s   g                        b   n   t   r   i   -
-       rst :   x   .   w   z                        p   h   m   k   j   rst
+       --- :   x   .   w   z                        p   h   m   k   j   ---
                        SYS sft SYM              ret spc ---
        */
            __ , KC_QUOT, KC_COMM,    KC_U,   KC_C,  KC_V,     KC_Q,  KC_F,  KC_D,  KC_L,  KC_Y,   KC_SLASH,
    SWITCH_LANG,   GUI_A,   ALT_O,   LT3_E,  CTL_S,  KC_G,     KC_B,  CTL_N, LT3_T, ALT_R, GUI_I,  KC_MINUS,
-       QK_BOOT, KC_SCLN,    KC_X,  KC_DOT,   KC_W,  KC_Z,     KC_P,  KC_H,  KC_M,  KC_K,  KC_J,   QK_BOOT,
+       XX_FAKE, KC_SCLN,    KC_X,  KC_DOT,   KC_W,  KC_Z,     KC_P,  KC_H,  KC_M,  KC_K,  KC_J,   XX_FAKE,
                              MO_SYS , OSM_SFT , OSL_SYM ,     KC_ENTER , KC_SPC,  __                     ),
 
   [L_QWERTY] = LAYOUT_split_3x6_3(
